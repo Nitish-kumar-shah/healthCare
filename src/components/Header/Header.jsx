7d6264d5b80cd80b1385ee8e886c915aa246240f
Nitish kumar shah <nitish.zyn@gmail.com>
2025-04-05 11:25:46 +0530

    created header and footer

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Container, Logo, LogoutBtn } from "../index";
+import { useSelector } from "react-redux";
+
+const Header = () => {
+  const authStatus = useSelector((state) => state.auth.status);
+
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: authStatus,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "About",
+      slug: "/about",
+      active: authStatus,
+    },
+    {
+      name: "chat-bot",
+      slug: "/chat-bot",
+      active: authStatus,
+    },
+  ];
+
+  return (
+    <header className="py-3 shadow bg-gray-500 ">
+      <Container>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo width="70px" />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
